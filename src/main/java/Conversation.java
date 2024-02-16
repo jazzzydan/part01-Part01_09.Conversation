26262657ae08c79580af816709b85d4b30dca7b7
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 11:38:10 +0200

    Initial commit

---
+++ src/main/java/Conversation.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Greetings! How are you doing?");
+        String second = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String third = scanner.nextLine();
+
+        System.out.println("Thanks for sharing!");
+
+    }
+}
